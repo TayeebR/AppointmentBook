8316d310b0e5d093da2228c0fa268100608b2dd9
TayeebR <tayeebr@nycstudents.net>
2025-03-12 13:15:19 +0000

    Final.

--- Main.java
+++ Main.java
@@ -1,1 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        boolean [] [] schedule = new boolean [8] [60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for (int i = 10; i < 15; i++) schedule[1] [i] = true;
+        for (int i = 30; i < 45; i++) schedule[1] [i] = true;
+        for (int i = 50; i < 60; i++) schedule[1] [i] = true;
+        a.printPeriod(2);
+        System.out.println(a.findFreeBlock(2, 15));
+       System.out.println(a.findFreeBlock(2, 9)); 
+        System.out.println(a.findFreeBlock(2, 20));
+        schedule = new boolean [8] [60];
+        for (int i = 25; i < 30; i++) schedule[1] [i] = true;
+        for (int i = 0; i < 15; i++) schedule[2] [i] = true;
+        for (int i = 41; i < 60; i++) schedule[2] [i] = true;
+        for (int i = 5; i < 30; i++) schedule[3] [i] = true;
+        for (int i = 44; i < 60; i++) schedule[3] [i] = true;
+        AppointmentBook b = new AppointmentBook(schedule);
+        int period = 2; 
+        while (period < 5) {
+            System.out.println("period" + period);
+            b.printPeriod(period);
+            period++;
+        }
+        b.makeAppointment(2, 4, 22);
+        b.makeAppointment(3, 4, 3);
+        b.makeAppointment(2, 4, 30);
+        b.printPeriod(2);
+        System.out.println();
+        b.printPeriod(3);
+        System.out.println();
+        b.printPeriod(4);
+    }
+}
